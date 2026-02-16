4361c4504c33581f38894e405fa2f1b3358fc688
iscotm <iscotavaresm@gmail.com>
2026-02-16 20:52:31 -0300

    chore: Update Supabase instance URL and anonymous key, and add them to a new .env file.

--- create_users.ts
+++ create_users.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rfzhyhsxepojwfrzknie.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmemh5aHN4ZXBvandmcnprbmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MjY1MTYsImV4cCI6MjA4NTMwMjUxNn0.FP_nfnuPkCHdfNCRiyLAIaZ1WEjiHx7CvFgi0o1lilc';
+const supabaseUrl = 'https://lykjeuuguglwujknhhhw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5a2pldXVndWdsd3Vqa25oaGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NjUxODYsImV4cCI6MjA4NjI0MTE4Nn0.Z0kxmygufP23YLTVpqLduoQ1rI740N5tv10HFZlQTPw';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rfzhyhsxepojwfrzknie.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmemh5aHN4ZXBvandmcnprbmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MjY1MTYsImV4cCI6MjA4NTMwMjUxNn0.FP_nfnuPkCHdfNCRiyLAIaZ1WEjiHx7CvFgi0o1lilc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://lykjeuuguglwujknhhhw.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5a2pldXVndWdsd3Vqa25oaGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NjUxODYsImV4cCI6MjA4NjI0MTE4Nn0.Z0kxmygufP23YLTVpqLduoQ1rI740N5tv10HFZlQTPw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
